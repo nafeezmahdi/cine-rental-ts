cf1047dca19d2070aa7b2fd250dd2f84ca87ab67
nafeezmahdi <nafeezmahdi09@gmail.com>
2025-07-26 23:17:11 +0600

    header sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <SideBar />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
