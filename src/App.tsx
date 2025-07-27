b2c1d8a206123fd9b03015a10a6d839ba245e1e0
nafeezmahdi <nafeezmahdi09@gmail.com>
2025-07-27 22:48:22 +0600

    movie list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import MovieList from "./components/MovieList";
 import SideBar from "./components/SideBar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <SideBar />
+          <MovieList />
         </div>
       </main>
     </>
