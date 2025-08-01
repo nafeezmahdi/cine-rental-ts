05e57058c26d95f1c67aa0ff57b0b6592fe9342b
nafeezmahdi <nafeezmahdi09@gmail.com>
2025-08-01 21:58:31 +0600

    c add

--- src/components/MovieCart/CartDetails.tsx
+++ src/components/MovieCart/CartDetails.tsx
@@ -10,7 +10,20 @@ interface CartDetailsProps {
 }
 
 export default function CartDetails({ onClose }: CartDetailsProps) {
-  const { cartData } = useContext(MovieContext);
+  const { cartData, setCartData } = useContext(MovieContext);
+
+  function handleDeleteCart(
+    evnt: React.MouseEvent<HTMLButtonElement>,
+    itemId: string
+  ) {
+    evnt.preventDefault();
+
+    const filteredItem = cartData.filter((item) => {
+      return item.id !== itemId;
+    });
+
+    setCartData([...filteredItem]);
+  }
 
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
@@ -20,34 +33,41 @@ export default function CartDetails({ onClose }: CartDetailsProps) {
             Your Carts
           </h2>
           <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
-            {cartData.map((item: MovieCardData) => (
-              <div className="grid grid-cols-[1fr_auto] gap-4">
-                <div className="flex items-center gap-4">
-                  <img
-                    className="rounded overflow-hidden"
-                    src={getImgUrl(item.cover)}
-                    alt={item.cover}
-                    width={"50px"}
-                    height={"50px"}
-                  />
-                  <div>
-                    <h3 className="text-base md:text-xl font-bold">
-                      {item.title}
-                    </h3>
-                    <p className="max-md:text-xs text-[#575A6E]">
-                      {item.genre}
-                    </p>
-                    <span className="max-md:text-xs">${item.price}</span>
+            {cartData.length === 0 ? (
+              <p className="text-3xl">The Cart is Empty!!!</p>
+            ) : (
+              cartData.map((item: MovieCardData) => (
+                <div key={item.id} className="grid grid-cols-[1fr_auto] gap-4">
+                  <div className="flex items-center gap-4">
+                    <img
+                      className="rounded overflow-hidden"
+                      src={getImgUrl(item.cover)}
+                      alt={item.cover}
+                      width={"50px"}
+                      height={"50px"}
+                    />
+                    <div>
+                      <h3 className="text-base md:text-xl font-bold">
+                        {item.title}
+                      </h3>
+                      <p className="max-md:text-xs text-[#575A6E]">
+                        {item.genre}
+                      </p>
+                      <span className="max-md:text-xs">${item.price}</span>
+                    </div>
+                  </div>
+                  <div className="flex justify-between gap-4 items-center">
+                    <button
+                      className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white"
+                      onClick={(evnt) => handleDeleteCart(evnt, item.id)}
+                    >
+                      <img className="w-5 h-5" src={Delete} alt="remove" />
+                      <span className="max-md:hidden">Remove</span>
+                    </button>
                   </div>
                 </div>
-                <div className="flex justify-between gap-4 items-center">
-                  <button className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white">
-                    <img className="w-5 h-5" src={Delete} alt="remove" />
-                    <span className="max-md:hidden">Remove</span>
-                  </button>
-                </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
           <div className="flex items-center justify-end gap-2">
             <a
